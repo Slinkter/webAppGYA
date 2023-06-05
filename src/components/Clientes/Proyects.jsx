806da5b391b0eab8d3dfbfe9db2e85230cb2a25e
jhonnycave <luis.j.cueva@gmail.com>
2023-06-05 14:31:15 -0500

    proyect/obra12

--- src/components/Clientes/Proyects.jsx
+++ src/components/Clientes/Proyects.jsx
@@ -12,131 +12,141 @@ import obra08 from "../../img/imgobra/obra08.jpg";
 import obra09 from "../../img/imgobra/obra09.jpg";
 import obra10 from "../../img/imgobra/obra10.jpg";
 import obra11 from "../../img/imgobra/obra11.jpg";
+import obra12 from "../../img/imgobra/obra12.jpg";
 import { Obra } from "./Obra";
 
 function Proyects() {
-    //image, residencial, name, address, numdpto, year
-    const listProject = [
-        {
-            id: 1,
-            image: obra01,
-            residencial: "Residencial Miraflores",
-            name: "Inversiones Beraca S.A.C.",
-            address: "Miraflores",
-            numdpto: "20",
-            year: "Mayo 2012",
-        },
-        {
-            id: 2,
-            image: obra02,
-            residencial: "Mirador de la Reserva",
-            name: "Inmobiliaria Beraca & Bostad S.A.C.",
-            address: "Lince",
-            numdpto: "50",
-            year: "Octubre 2015",
-        },
-        {
-            id: 3,
-            image: obra03,
-            residencial: "Residencial Bambamarca",
-            name: "Personal Natural",
-            address: "Cajamarca",
-            numdpto: "21",
-            year: "Enero 2018",
-        },
-        {
-            id: 4,
-            image: obra04,
-            residencial: "Residencial Edificación  ",
-            name: "GHS CONSTRUCTORA S.A.C.",
-            address: "San Jua de Miraflores",
-            numdpto: "26",
-            year: "Noviembre 2020",
-        },
-        {
-            id: 5,
-            image: obra05,
-            residencial: "Residencial Puerta verde ",
-            name: "Grupo DyM Constructora S.A.C.",
-            address: "Ate  ",
-            numdpto: "18",
-            year: "Junio 2019",
-        },
-        {
-            id: 6,
-            image: obra06,
-            residencial: "Residencial Prado. ",
-            name: "Grupo DyM Constructora S.A.C.",
-            address: "Ate  ",
-            numdpto: "24",
-            year: "Febrero 2020",
-        },
-        {
-            id: 7,
-            image: obra07,
-            residencial: "Residencial Monterrico Alto",
-            name: "Grupo DyM Constructora S.A.C.",
-            address: "Ate  ",
-            numdpto: "24",
-            year: "Junio 2021 ",
-        },
-        {
-            id: 8,
-            image: obra08,
-            residencial: "Residencial Magdalena",
-            name: "GHS CONSTRUCTORA S.A.C.",
-            address: "Magdalena",
-            numdpto: "12",
-            year: "Agosto de 2021",
-        },
-        {
-            id: 9,
-            image: obra09,
-            residencial: "Residencial Ventura",
-            name: "Urbanica Proyectos Inmobiliarios S.A.C.",
-            address: "San Miguel",
-            numdpto: "14",
-            year: "Febrero 2022",
-        },
-        {
-            id: 10,
-            image: obra10,
-            residencial: " Residencial Maddy",
-            name: "CONSTRUCTORA E INMOBILIARIA ICASA S.A.C",
-            address: "Magdalena del mar",
-            numdpto: "62",
-            year: "Mayo 2022 ",
-        },
-        {
-            id: 11,
-            image: obra11,
-            residencial: " Residencial Vidamor",
-            name: "Grupo DyM Constructora S.A.C.",
-            address: "ATE",
-            numdpto: "60",
-            year: "Diciembre 2022 ",
-        },
-    ];
-    return (
-        <section className="bg_servicios">
-            <div className="container py-5">
-                <div className=" mx-auto space_servicio text-center ">
-                    <h2 className=" animated fadeIn  mt-3 mb-2">Proyectos</h2>
+  //image, residencial, name, address, numdpto, year
+  const listProject = [
+    {
+      id: 1,
+      image: obra01,
+      residencial: "Residencial Miraflores",
+      name: "Inversiones Beraca S.A.C.",
+      address: "Miraflores",
+      numdpto: "20",
+      year: "Mayo 2012",
+    },
+    {
+      id: 2,
+      image: obra02,
+      residencial: "Mirador de la Reserva",
+      name: "Inmobiliaria Beraca & Bostad S.A.C.",
+      address: "Lince",
+      numdpto: "50",
+      year: "Octubre 2015",
+    },
+    {
+      id: 3,
+      image: obra03,
+      residencial: "Residencial Bambamarca",
+      name: "Personal Natural",
+      address: "Cajamarca",
+      numdpto: "21",
+      year: "Enero 2018",
+    },
+    {
+      id: 4,
+      image: obra04,
+      residencial: "Residencial Edificación  ",
+      name: "GHS CONSTRUCTORA S.A.C.",
+      address: "San Jua de Miraflores",
+      numdpto: "26",
+      year: "Noviembre 2020",
+    },
+    {
+      id: 5,
+      image: obra05,
+      residencial: "Residencial Puenta Verde",
+      name: "Grupo DyM Constructora S.A.C.",
+      address: "Ate  ",
+      numdpto: "18",
+      year: "Junio 2019",
+    },
+    {
+      id: 6,
+      image: obra06,
+      residencial: "Residencial El Prado",
+      name: "Grupo DyM Constructora S.A.C.",
+      address: "Ate  ",
+      numdpto: "24",
+      year: "Febrero 2020",
+    },
+    {
+      id: 7,
+      image: obra07,
+      residencial: "Residencial Monterrico Alto",
+      name: "Grupo DyM Constructora S.A.C.",
+      address: "Ate  ",
+      numdpto: "24",
+      year: "Junio 2021 ",
+    },
+    {
+      id: 8,
+      image: obra08,
+      residencial: "Residencial Magdalena",
+      name: "GHS CONSTRUCTORA S.A.C.",
+      address: "Magdalena",
+      numdpto: "12",
+      year: "Agosto de 2021",
+    },
+    {
+      id: 9,
+      image: obra09,
+      residencial: "Residencial Ventura",
+      name: "Urbanica Proyectos Inmobiliarios S.A.C.",
+      address: "San Miguel",
+      numdpto: "14",
+      year: "Febrero 2022",
+    },
+    {
+      id: 10,
+      image: obra10,
+      residencial: " Residencial Maddy",
+      name: "CONSTRUCTORA E INMOBILIARIA ICASA S.A.C",
+      address: "Magdalena del mar",
+      numdpto: "62",
+      year: "Mayo 2022 ",
+    },
+    {
+      id: 11,
+      image: obra11,
+      residencial: " Residencial Vidamor",
+      name: "Grupo DyM Constructora S.A.C.",
+      address: "ATE",
+      numdpto: "60",
+      year: "Diciembre 2022 ",
+    },
+    {
+      id: 12,
+      image: obra12,
+      residencial: " Residencial Elenor",
+      name: "P&D Holding Group S.A.C.",
+      address: "Magdalena del mar",
+      numdpto: "32",
+      year: "Junio 2023 ",
+    },
+  ];
+  return (
+    <section className="bg_servicios">
+      <div className="container py-5">
+        <div className=" mx-auto space_servicio text-center ">
+          <h2 className=" animated fadeIn  mt-3 mb-2">Proyectos</h2>
 
-                    <p className="animated fadeIn  lead text-secondary mt-2">
-                        <hr className="  mx-3 " />
-                        Nuestros servicios de instalación cuenta con productos y
-                        accesorios que cumple con los estándares de calidad.
-                    </p>
-                </div>
-                <section className="products animated fadeIn">
-                    {listProject.map((servicio) => {
-                        return <Obra key={servicio.id} {...servicio} />;
-                    })}
-                </section>
-            </div>
+          <p className="animated fadeIn  lead text-secondary mt-2">
+            <hr className="  mx-3 " />
+            Nuestros servicios de instalación cuenta con productos y accesorios
+            que cumple con los estándares de calidad.
+          </p>
+        </div>
+        <section className="products animated fadeIn">
+          {listProject.map((servicio) => {
+            return <Obra key={servicio.id} {...servicio} />;
+          })}
         </section>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default Proyects;
