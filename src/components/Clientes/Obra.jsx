8252c14827d666572fd14f901500a939b9d7a5a6
jhonnycave <luis.j.cueva@gmail.com>
2023-06-05 17:35:19 -0500

    mejoras en css y img

--- src/components/Clientes/Obra.jsx
+++ src/components/Clientes/Obra.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import defaultImage from "../../img/calidad.jpg";
 
-
 import "./Obra.css";
 
 function Obra(props) {
-    const { image, residencial, name, address, numdpto, year } = props;
-    return (
-        <article className="obra grow">
-            <img src={image || defaultImage} alt={residencial} />
-          
-            <h4 className="mt-4 ">{residencial || "residencial"}</h4>
-            <hr className=" mx-5 "/>
-            <p className="m-1 ">{name || "empresa"}</p>
-            <p className="m-1 ">{address || "dirección"}</p>
-          
-            <p className=" mb-3 ">{year || "año"}</p>
-        </article>
-    );
+  const { image, residencial, name, address, numdpto, year } = props;
+  return (
+    <article className="obra grow">
+      <img src={image || defaultImage} alt={residencial} />
+      <h4 className="mt-3 ">{residencial || "residencial"}</h4>
+      <hr className="m-1 mx-5" />
+      <p className="m-1">{name || "empresa"}</p>
+      <p className="m-1">{address || "dirección"}</p>
+      <p className="mb-3">{year || "año"}</p>
+    </article>
+  );
 }
 
 export { Obra };
--- src/components/FooterWrapper.jsx
+++ src/components/FooterWrapper.jsx
@@ -1,51 +1,51 @@
 import "./FooterWrapper.css";
 
 export default function Footer() {
-    return (
-        <section>
-            <footer>
-                <div class="footer-section">
-                    <div class="container">
-                        <div class="row flexcontainer">
-                            <div class="col-md-4 ">
-                                <h2 class="titleh2footer">Contactos</h2>
-                                <p className="textwhite">
-                                    <div>
-                                        <div>
-                                            <i class="fa-brands fa-whatsapp mx-2 mb-2"></i>
-                                            971-870-665
-                                        </div>
-                                        <div>
-                                            <i class="fa-brands fa-whatsapp mx-2"></i>
-                                            974-278-303
-                                        </div>
-                                    </div>
-                                </p>
-                            </div>
-                            <div class="col-md-4 ">
-                                <h2 class="titleh2footer">Horarios</h2>
-                                <p className="textwhite">
-                                    <i class="fa fa-calendar mx-2 mb-2"></i>
-                                    Lunes a Sábado
-                                    <p>9:00 am - 5:00 pm</p>
-                                </p>
-                            </div>
-                            <div class="col-md-4 ">
-                                <h2 class="titleh2footer">Ubicación</h2>
-                                <p className="textwhite">
-                                    Av. Los Frenos 1250
-                                    <p> La Molina</p>
-                                </p>
-                            </div>
-                        </div>
+  return (
+    <section>
+      <footer>
+        <div class="footer-section">
+          <div class="container">
+            <div class="row flexcontainer">
+              <div class="col-md-4 ">
+                <h2 class="titleh2footer">Contactos</h2>
+                <p className="textwhite">
+                  <div>
+                    <div>
+                      <i class="fa-brands fa-whatsapp mx-2 mb-2"></i>
+                      971-870-665
                     </div>
-                </div>
-            </footer>
-
-            <div className=" mt-4 text-center">
-                <p>Lima - Peru</p>
-                <p>Copyright ©2022</p>
+                    <div>
+                      <i class="fa-brands fa-whatsapp mx-2"></i>
+                      974-278-303
+                    </div>
+                  </div>
+                </p>
+              </div>
+              <div class="col-md-4 ">
+                <h2 class="titleh2footer">Horarios</h2>
+                <p className="textwhite">
+                  <i class="fa fa-calendar mx-2 mb-2"></i>
+                  Lunes a Sábado
+                  <p>9:00 am - 5:00 pm</p>
+                </p>
+              </div>
+              <div class="col-md-4 ">
+                <h2 class="titleh2footer">Ubicación</h2>
+                <p className="textwhite">
+                  Av. Los Frenos 1250
+                  <p> La Molina</p>
+                </p>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </footer>
+
+      <div className=" mt-4 text-center">
+        <p>Lima - Peru</p>
+        <p>Copyright ©2023</p>
+      </div>
+    </section>
+  );
 }
