f0170c9ec4f62151fdad6b9d2a313d051b3d5db7
Slinkter <luis.j.cueva@gmail.com>
2024-01-09 22:06:42 -0500

    update

--- src/components/FooterWrapper.jsx
+++ src/components/FooterWrapper.jsx
@@ -2,47 +2,49 @@ import "./FooterWrapper.css";
 
 export default function Footer() {
   return (
-    <section className="text-center">
-      <footer className="mb-3">
-        <div className="footer-section">
-          <div className="container">
-            <div className="row flexcontainer">
-              <div className="col-md-4 ">
-                <h2 className="titleh2footer">CONTACTO</h2>
-                <p classNameName="">
-                  <div>
-                    <div className="">
-                      <i className="fa-brands fa-whatsapp mx-2 mb-2 "></i>
-                      996-537-435
-                    </div>
-                    <div className="">
-                      <i className="fa-brands fa-whatsapp mx-2"></i>
-                      971-870-665
-                    </div>
-                  </div>
-                </p>
+    <section className="text-center ">
+      {/*  */}
+      <footer className="mb-3  bg__footer">
+        <div className="row m-0">
+          <div className="col-md-4 my-2">
+            <h2 className="footer__h2">CONTACTO</h2>
+            <div>
+              <div className="">
+                <i className="fa-brands fa-whatsapp mx-2"></i>
+                996-537-435
               </div>
-              <div className="col-md-4 ">
-                <h2 className="titleh2footer">HORARIOS</h2>
-                <div classNameName="textwhite">
-                  <i className="fa fa-calendar mx-2 mb-2 "></i>
-                  Lunes a Sábado
-                  <br />
-                  9:00 am - 5:00 pm
-                </div>
+              <div className="">
+                <i className="fa-brands fa-whatsapp mx-2"></i>
+                971-870-665
               </div>
-              <div className="col-md-4 ">
-                <h2 className="titleh2footer">DIRECCIÓN</h2>
-                <p classNameName="textwhite">
-                  Av. Los Fresnos MZ. H LT. 16
-                  <p> La Molina </p>
-                </p>
-              </div>
+            </div>
+          </div>
+          <div className="col-md-4 my-2">
+            <h2 className="footer__h2">HORARIOS</h2>
+            <div>
+              <i className="fa fa-calendar mx-2 mb-2 "></i>
+              Lunes a Sábado
+            </div>
+            <div>
+              <i className="fa-regular fa-clock mx-2 mb-2"></i>
+              9:00 am - 5:00 pm
+            </div>
+          </div>
+          <div className="col-md-4 my-2">
+            <h2 className="footer__h2">DIRECCIÓN</h2>
+            <div>
+              <i className="fa-solid fa-location-dot mx-2 mb-2"></i>
+              Av. Los Fresnos MZ. H LT. 16
+            </div>
+
+            <div>
+              <i className="fa-regular fa-map mx-2 mb-2"></i>
+              La Molina
             </div>
           </div>
         </div>
       </footer>
-
+      {/*  */}
       <div>
         <p>Copyright ©2024</p>
       </div>
